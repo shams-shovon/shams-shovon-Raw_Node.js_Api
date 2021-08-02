3bdd1b066d259c977a241ca92dbb453b3a12224b
shams-shovon <17-34487-2@student.aiub.edu>
2021-08-02 13:09:03 +0600

    record the requested body

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ Date: 02-08-2021
 
 //dependencies
 const http = require('http');
+const url = require('url');
+const {StringDecoder} = require('string_decoder');
 
 //app object - module scaffolding
 const app = {};
@@ -22,14 +24,39 @@ app.createServer = ()=>
   const server = http.createServer(app.handleReqRes);
   server.listen(app.config.port, () =>
   {
-    console.log(`listing port ${app.config.port}`);
+    console.log(`listening port ${app.config.port}`);
   });
 }
 
 //handle Request Response
 app.handleReqRes = (req,res) =>
 {
-  res.end("It's me");
+  //request handling
+  const parseUrl = url.parse(req.url, true);
+  const path = parseUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+  const method = req.method.toLowerCase();
+  const queryStringObject = parseUrl.query;
+  const headerObject = req.headers;
+  console.log(headerObject);
+
+  //buffer to String. It's same to "toString()"
+  const decoder = new StringDecoder('utf-8');
+  let realData = '';
+  req.on('data',(buffer) =>
+  {
+    realData += decoder.write(buffer);
+  });
+
+  req.on('end',() =>
+  {
+    realData += decoder.end();
+    console.log(realData);
+
+    //response handling
+    res.end("It's me.");
+  });
+
 }
 
 //start Server
